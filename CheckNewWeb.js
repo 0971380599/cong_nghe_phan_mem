08ed1ef1c282a6ec0892b287b473f2a3982ac073
0971380599 <81423789+0971380599@users.noreply.github.com>
2021-05-15 16:46:47 +0700

    cong nghe phan mem

---
+++ CheckNewWeb.js
@@ -0,0 +1,44 @@
+function check() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam").href = "";
+    }
+}
+function check1() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam1").href = "";
+    }
+}
+function check2() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam2").href = "";
+    }
+}
+function check3() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam3").href = "";
+    }
+}function check4() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam4").href = "";
+    }
+}function check5() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam5").href = "";
+    }
+}function check6() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam6").href = "";
+    }
+}function check7() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam").href = "";
+    }
+}
