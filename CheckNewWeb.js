d3e23948e419bd5dd22028e0a773ad35b9d429ad
0971380599 <81423789+0971380599@users.noreply.github.com>
2021-05-17 20:55:36 +0700

    cong nghe phan mem

--- CheckNewWeb.js
+++ CheckNewWeb.js
@@ -1,4 +1,4 @@
-function check() {
+function check(a) {
     let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
     if (check === false) {
         document.getElementById("howkteam").href = "";
@@ -39,6 +39,66 @@ function check3() {
 }function check7() {
     let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
     if (check === false) {
-        document.getElementById("howkteam").href = "";
+        document.getElementById("howkteam7").href = "";
+    }
+}function check8() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam8").href = "";
+    }
+}function check9() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam9").href = "";
+    }
+}function check10() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam10").href = "";
+    }
+}function check11() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam11").href = "";
+    }
+}function check12() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam12").href = "";
+    }
+}function check13() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam13").href = "";
+    }
+}function check14() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam14").href = "";
+    }
+}function check15() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam15").href = "";
+    }
+}function check16() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam16").href = "";
+    }
+}function check17() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam17").href = "";
+    }
+}function check18() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam18").href = "";
+    }
+}function check19() {
+    let check = confirm("Bạn có muốn chuyển sang liên kiết mới?");
+    if (check === false) {
+        document.getElementById("howkteam19").href = "";
     }
 }
